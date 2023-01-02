7e4a42af4c03aa113ae8119081a98654f2e54a69
Ayodele Alakija <alakijaayo@gmail.com>
2023-01-02 20:34:20 +0000

    Add folders

---
+++ src/main/java/soulradio/soulradio/SoulradioApplication.java
@@ -0,0 +1,13 @@
+package soulradio.soulradio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoulradioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoulradioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/soulradio/soulradio/SoulradioApplicationTests.java
@@ -0,0 +1,13 @@
+package soulradio.soulradio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoulradioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
