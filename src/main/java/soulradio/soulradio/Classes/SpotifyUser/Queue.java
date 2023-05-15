615e76d9281bbcb30bebaf2234568116d71010ba
Ayodele Alakija <alakijaayo@gmail.com>
2023-05-15 19:04:53 +0100

    Sorting array after voting

--- src/main/java/soulradio/soulradio/Classes/SpotifyUser/Queue.java
+++ src/main/java/soulradio/soulradio/Classes/SpotifyUser/Queue.java
@@ -1,5 +1,7 @@
 package soulradio.soulradio.Classes.SpotifyUser;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
@@ -33,6 +35,27 @@ public class Queue {
     return queuedTracks;
   }
 
+  public ArrayList<JSONObject> sortQueuedTracks(ArrayList<JSONObject> jsonArray, String key, boolean ascending) {
+    Collections.sort(jsonArray, new Comparator<JSONObject>() {
+      @Override
+      public int compare(JSONObject jsonObject1, JSONObject jsonObject2) {
+        Object value1 = jsonObject1.get(key);
+        Object value2 = jsonObject2.get(key);
+          
+        if (value1 instanceof Integer && value2 instanceof Integer) {
+          return ascending ? ((Integer) value2).compareTo((Integer) value1) : ((Integer) value1).compareTo((Integer) value2);
+      } else if (value1 instanceof Long && value2 instanceof Long) {
+          return ascending ? ((Long) value2).compareTo((Long) value1) : ((Long) value1).compareTo((Long) value2);
+      } else {
+          int compareResult = Long.compare(((Number) value1).longValue(), ((Number) value2).longValue());
+          return ascending ? -compareResult : compareResult;
+      }
+      }
+  });
+
+  return jsonArray;
+  }
+
   public String getStringValue(String Track, String key) {
     JSONObject track =  getTrackDetails(Track);
     Object trackDetails = track.get(key);
@@ -42,14 +65,15 @@ public class Queue {
 
   public ArrayList<JSONObject> updateQueuedTracks(int number, String vote, String key) {
     int count = Integer.parseInt(queuedTracks.get(number).get(key).toString());
-    int updatedCount = count + 1;
+    Integer updatedCount = count + 1;
 
     if (vote.equals("up")) {
       queuedTracks.get(number).replace("votesUp", updatedCount);
     } else {
       queuedTracks.get(number).replace("votesDown", updatedCount);
     }
-    return queuedTracks;
+    
+    return sortQueuedTracks(queuedTracks, "votesUp", true);
   }
 
   public JSONObject getTrack() {
